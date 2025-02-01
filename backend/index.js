bcfc12b6d4e475be0b6b035231a4eee00db4ab01
Muhammad Faisal <digital.1faisal@gmail.com>
2025-02-01 08:38:37 +0000

    add simple auth middleware

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,26 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import process from "process";
+import auth from "./auth/user.auth.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors());
+// CORS configuration
+// const corsOptions = {
+//   origin: 'http://your-frontend-domain.com', // Replace with your frontend domain
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+// };
+// app.use(cors(corsOptions));
+
 
 app.get("/", (req, res) => {
   res.status(200).json({ msg: "Server running" });
+});
+
+app.get("/blog", auth, (req, res) => {
+  res.status(200).json({ msg: "Authenticated" });
 });
 
 app.listen(port, () => {
